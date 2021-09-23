f58c20e636587cec8037b67f9f6534e90ce4f61b
HeitorCervantes <42389629+HeitorCervantes@users.noreply.github.com>
2021-09-23 09:17:32 -0300

    Add files via upload

---
+++ DescriçãoYOCTO.py
@@ -0,0 +1,22 @@
+#YOCTO
+
+#- Amplamente difundido, existindo uma grande comunidade de desenvolvedores e ambientes de desenvolvimento, além da grande variedade de livros e materiais de treinamento.
+
+#- Construído em camadas, o que possibilita a inserção de funções adicionais no sistema, ou a criação de personalizações exclusivas.
+
+#- Suporte amplo de diversos fabricantes de hardware.
+
+#- Alta flexibilidade e personalização, com a possibilidade de sobreposição de camadas.
+
+#- Longa curva de aprendizagem, devido à complexidade e variedade de ferramentas.
+
+#- Tempo alto de desenvolvimento, devido ao grande número de pacotes a serem construídos.
+
+#- Necessidade de hardware mais robusto para sua utilização.
+
+#- Cache embutido que permite reutilização de componentes anteriormente construídos, para otimização dos processos de construção.
+
+#- Alta aplicabilidade.
+
+#Aplicações: Utilização em sistemas mais robustos, com disponibilidade de processamento de dados, utilização em multiplataformas, sistemas com maior complexibilidade e 
+#necessidade de mais ferramentas.
